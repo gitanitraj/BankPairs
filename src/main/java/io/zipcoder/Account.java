db7abec55d9022b5171e9dcc617f976831f2f0d8
gitanitraj <anitra.com@gmail.com>
2026-04-29 11:23:33 -0400

    Implemented Acct Biz Person updates

--- src/main/java/io/zipcoder/Account.java
+++ src/main/java/io/zipcoder/Account.java
@@ -1,47 +1,73 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public abstract class Account {
     private Object accountHolder;
     private Double balance;
     private String accountNumber;
+     
     // TODO: Add a field to track transactions
+    private ArrayList<String> transactionHistory;
 
     public Account(Object accountHolder, Double balance, String accountNumber) {
         // TODO: Implement constructor
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+        this.transactionHistory = new ArrayList<>();
     }
 
     public Object getAccountHolder() {
         // TODO: Implement getter
-        return null;
+        return accountHolder;
     }
 
     public Double getBalance() {
         // TODO: Implement getter
-        return null;
+        return balance;
     }
 
     public void setBalance(Double balance) {
         // TODO: Implement setter
+        this.balance = balance;
     }
 
     public String getAccountNumber() {
         // TODO: Implement getter
-        return null;
+        return accountNumber;
     }
 
     public void credit(Double amount) {
         // TODO: Implement credit method (add money to account)
         // TODO: Record this transaction
-    }
+        if (amount > 0) {
+            this.balance += amount;
+
+            transactionHistory.add("Credit: +" + amount + " | New Balance: " + this.balance);
+        }
+        else {
+                throw new IllegalArgumentException("Credit amount must be positive");
+            }
+        }
 
     public void debit(Double amount) {
         // TODO: Implement debit method (remove money from account)
         // TODO: Record this transaction
+        if (amount > 0 && balance >= amount) {
+            this.balance -= amount;
+            transactionHistory.add("Debit: -" + amount + " | New Balance: " + this.balance);
+            System.out.println("New Balance " + this.balance);
+        }
+        else {
+            throw new IllegalArgumentException("Debit amount must be positive");
+        } 
+        
     }
 
     public Object getTransactions() {
         // TODO: Implement method to return transaction history
-        return null;
+        return transactionHistory;
     }
 }
 
