1a0cfcf117deb635e4fc3abec8eec6d30ab55ded
gitanitraj <anitra.com@gmail.com>
2026-04-29 16:24:43 -0400

    Updated InvestmentAccount Class

--- src/main/java/io/zipcoder/InvestmentAccount.java
+++ src/main/java/io/zipcoder/InvestmentAccount.java
@@ -6,26 +6,34 @@ public class InvestmentAccount extends Account {
     public InvestmentAccount(Object accountHolder, Double balance, String accountNumber, Double interestRate) {
         super(accountHolder, balance, accountNumber);
         // TODO: Implement constructor
-    }
+        this.interestRate = interestRate;
+    }   
 
     public Double getInterestRate() {
         // TODO: Implement getter
-        return null;
+        return interestRate;
     }
 
     public void setInterestRate(Double interestRate) {
         // TODO: Implement setter
+        this.interestRate = interestRate;
     }
 
     public void applyInterest() {
         // TODO: Implement method to apply interest to the balance
         // New balance = current balance + (current balance * interest rate)
         // Note: Interest applies even to negative balances
+        Double interestAmount = getBalance() * interestRate;
+
+        setBalance(getBalance() + interestAmount);
+
     }
 
     @Override
     public void debit(Double amount) {
         // TODO: Implement debit method
         // Investment accounts do NOT have overdraft protection - allow balance to go negative
+        super.debit(amount);
     }
+
 }
